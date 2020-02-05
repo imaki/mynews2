5bed3b3e91dc0f44d0b47d1eb287a3c71d60fab4
imaki <ec2-user@ip-172-31-31-18.us-east-2.compute.internal>
2020-02-05 13:03:38 +0000

    php10

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,42 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+       <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
+
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+
+<form action="submit.php" method="post">
+    <p>性別：
+        <input type="radio" name="gender" value="男性"> 男性
+        ／
+        <input type="radio" name="gender" value="女性"> 女性
+    </p>
+
+    <input type="text" name="my_name" value="初期値">
+
+    <input type="checkbox" name="agree" value="on"> 同意する
+
+    <select name="pref">
+            <option value="hokkaido">北海道</option>
+            <option value="aomori">青森県</option>
+        </select>
+
+    <input type="submit" value="送信する">
+</form>
+
+</main>
+</body>    
 </html>
